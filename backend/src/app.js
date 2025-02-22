b5364ff6aa7b4fe52d45ef69058e6bc070575ce2
EduardoArauj0 <eduardoaraujo.contato13@gmail.com>
2025-02-22 11:37:25 -0300

    chore: cria estrutura de pastas frontend e adiciona HTML inicial

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const router = require("./routes/router");
 const cors = require ('cors')
+const router = require("./routes/router");
 
 const app = express();
 
